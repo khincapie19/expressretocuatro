6d983174a093e1d89b66e78b7aa3dc61bac9e9ee
Katherine <katherinehincapie19@hotmail.com>
2020-07-30 19:36:13 -0400

    Salùdame 2, otro intento

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/makers/:name', (req, res) => {
-  let param = req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1);
-  res.send(`<h1>Hola ${param}!</h1>`);
+  console.log(req.params.name)
+  let name = req.params.name ? req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1) : 'desconocido';
+  res.send(`<h1>Hola ${name}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
